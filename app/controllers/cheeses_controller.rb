6ed910fa334e9b2dfee06058fe9675511faef0cb
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-09-27 04:30:22 -0400

    commit tree 33e20f55127ce10b8a6a0a284d6ee7b7bddcf25e

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,11 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    #find a cheese using the ID from the url
+    cheese = Cheese.find_by(id: params[:id])
+
+    #sending a json response using the cheese object
+    render json: cheese
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+#To create a dynamic route, use the :param_name syntax as part of the route, such as get "/cheeses/:id", to: "cheeses#show".
+  get '/cheeses/:id', to: 'cheeses#show'
 end
